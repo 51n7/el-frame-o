cd55a61e5880e80575023430848a396a3baf2be2
Jon Best <smile@jonbest.ca>
2018-07-06 15:54:25 -0600

    removed unnecessary comments

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-// Modules to control application life and create native browser window
 const { app, globalShortcut, BrowserWindow, ipcMain } = require('electron')
 const fs = require('fs')
 const path = require('path')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
@@ -23,31 +20,15 @@ function createWindow () {
   })
   
   mainWindow.loadFile('index.html')
-  // mainWindow.loadURL('https://app.getflow.com/')
-  // mainWindow.loadURL('https://discordapp.com/')
-
-
+  
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+  mainWindow.on('closed', function () { mainWindow = null })
 }
 
-function execute () {
-  console.log('execute');
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
   
   const home = globalShortcut.register('CommandOrControl+Shift+H', () => {
     console.log('CommandOrControl+Shift+H is pressed')
-    // ipcRenderer.send('asynchronous-message', 'ping');
 
     // mainWindow.loadURL('https://app.getflow.com/')
     mainWindow.loadFile('index.html')
@@ -58,23 +39,13 @@ app.on('ready', () => {
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') { app.quit() } // On OS X it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+  if (mainWindow === null) { createWindow() } // On OS X it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 ipcMain.on('asynchronous-message', (event, arg) => {
   
